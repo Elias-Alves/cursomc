98ff2f79a30bb26661169777292945f3a5588116
Elias Alves <elias.ti.alves@gmail.com>
2019-01-31 20:45:20 -0200

    Endpoint /cliente/{id} disponivel

--- src/main/java/com/ealves/cursomc/domain/Cliente.java
+++ src/main/java/com/ealves/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.ealves.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente implements Serializable{
@@ -28,6 +29,8 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 
+	// O cliente pode serializar o endereco mais o endereco não pode serialize o cliente
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
